82517bf435f9566f8ef7fcf4cd4642580a2dbb52
Makarenkov Aleksandr <aarenaarenson@gmail.com>
2023-09-11 13:00:59 +0300

    manual test

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ mod planner;
 fn main() {
     let classes = game_data::extract("recipes.json");
 
-    let iron_rod = classes.iter().find(|cl| cl.contains_product("IronRod") && !cl.is_alternate);
-    if iron_rod.is_some() {
-        println!("{:?}", iron_rod.unwrap());
-    }
+    let iron_rod = classes.iter().filter(|cl| cl.contains_product("IronRod")).collect::<Vec<_>>();
 
+    for r in iron_rod {
+        println!("recipe name: {}", r.class_name);
+        println!("recipe ingredients: ");
+        for i in &r.ingredients {
+            println!("- {}", i.name);
+        }
+
+    }
 }
 
